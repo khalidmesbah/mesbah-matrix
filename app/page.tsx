25c33b18079272752d436204ee7cfea18f7a3844
khalidmesbah <swekhalidmesbah6@gmail.com>
2024-08-05 04:27:09 +0300

    add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+'use client';
+
+import { Tldraw } from 'tldraw';
+
+import { useTheme } from 'next-themes';
+import './index.css';
+
 export default function Home() {
-  return <>jfkdslf</>;
+  const { resolvedTheme } = useTheme();
+  return <Tldraw className="" inferDarkMode={resolvedTheme === 'dark'} />;
 }
