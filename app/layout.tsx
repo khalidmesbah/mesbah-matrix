54e24215cff59f58dec69344560da79bee1de3ca
khalidmesbah <swekhalidmesbah6@gmail.com>
2024-11-01 16:32:24 +0200

    feat: add speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import TopBarSkeleton from '@/components/skeletons/top-bar';
 import TopBarAuthWrapper from '@/components/top-bar-auth-wrapper';
 import { Toaster } from '@/components/ui/sonner';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { GeistMono } from 'geist/font/mono';
 import type { Metadata } from 'next';
 import { Suspense } from 'react';
@@ -40,10 +41,11 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
               </Suspense>
               {children}
             </div>
-            <Analytics />
           </main>
           <Toaster />
         </Providers>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
